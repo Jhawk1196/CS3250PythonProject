8c25979acd510262136fe35d66da3997e7ac3420
Justin Herrera <justinherrera@Justins-MacBook-Pro-2.local>
2020-03-03 19:07:15 -0700

    Added test case for display.py module and modified helloWorld() to return the value of the text in the tkinter window.

--- src/display.py
+++ src/display.py
@@ -4,5 +4,7 @@ from tkinter import *
 def helloWorld():
     root = Tk()
     w = Label(root, text="Hello, world!")
+    windowText = w.cget("text")
     w.pack()
     root.mainloop()
+    return windowText
