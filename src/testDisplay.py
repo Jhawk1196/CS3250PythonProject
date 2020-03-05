183de51921da04528fbc35ebe17a698616725528
Jarred Hawkins <jarred.hawkins07@gmail.com>
2020-03-04 18:29:35 -0700

    Revert "Fixed Merge Conflict"
    This reverts commit ef27dada6b354cde5ea0eed65897abad2c8a63c6, reversing
    changes made to bd94da2b983775d5962ec597f09af6af3142128c.

--- src/testDisplay.py
+++ src/testDisplay.py
@@ -1,8 +1,7 @@
 import unittest
 import src.display as display
 
+
 class TestHelloWorld(unittest.TestCase):
-    def test_Hello_World(self):
-        self.assertEqual('Hello, world!', display.helloWorld('Hello, world!'))
-    def test_Not_Hello_World(self):
-        self.assertNotEqual('Hello, world!', display.helloWorld('Goodbye, earth!'))
+    def runTest(self):
+        self.assertEqual('Hello, world!', display.helloWorld())
