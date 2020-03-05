fcb4329ef310a7fdde61d8c01e1aee581b5d00c8
Jarred Hawkins <jarred.hawkins07@gmail.com>
2020-03-05 11:33:30 -0700

    NO LONGER IN v0.1dev, NOW IN v0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PlaceholderName',
-    version='0.1dev',
+    version='0.1',
     packages=[open('requirements.txt').read()],
     license='MIT License',
     long_description=open('README.txt').read(),
