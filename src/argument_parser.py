e752e050c2ec687c3cf36aed7190fabe9a49bcd1
Steven Hulac <hulacst@icloud.com>
2020-03-22 14:37:00 -0600

    Added tests to CL argument parser

--- src/argument_parser.py
+++ src/argument_parser.py
@@ -1,4 +1,6 @@
 import argparse
+import unittest
+from sys import argv
 
 
 def parse_args():
@@ -18,8 +20,52 @@ This parses the command line arguments.
 We can easily add more later with more parser.add_argument.
 Examples: python main.py --url "http://rss.cnn.com/rss/cnn_us.rss"
           python main.py --file "some_file.json"
+          python main.py --config "my_config.yaml"
           python main.py --help
 
 The function will return an object with attributes .file and .url.
 If one isn't specified it's an empty string.
 """
+
+"""Here be the tests"""
+
+
+class ArgTests(unittest.TestCase):
+    def test_single_url(self):
+        argv[1:] = ["--url", "google.com"]
+        args = parse_args()
+        self.assertEqual(args.url, ['google.com'])
+        self.assertEqual(args.file, "")
+        self.assertEqual(args.config, "")
+
+    def test_single_file(self):
+        argv[1:] = ["--file", "some_file.json"]
+        args = parse_args()
+        self.assertEqual(args.file, ['some_file.json'])
+        self.assertEqual(args.url, "")
+        self.assertEqual(args.config, "")
+
+    def test_single_config(self):
+        argv[1:] = ["--config", "some_config.yaml"]
+        args = parse_args()
+        self.assertEqual(args.config, ['some_config.yaml'])
+        self.assertEqual(args.url, "")
+        self.assertEqual(args.file, "")
+
+    def test_multiple_url(self):
+        argv[1:] = ["--url", "google.com", "duckduckgo.com"]
+        args = parse_args()
+        self.assertEqual(args.url, ['google.com', "duckduckgo.com"])
+        self.assertEqual(args.file, "")
+        self.assertEqual(args.config, "")
+
+    def test_urls_and_files(self):
+        argv[1:] = ["--url", "google.com", "duckduckgo.com", "--file", "some_file.json", "another_file.json"]
+        args = parse_args()
+        self.assertEqual(args.url, ['google.com', "duckduckgo.com"])
+        self.assertEqual(args.file, ["some_file.json", "another_file.json"])
+        self.assertEqual(args.config, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
