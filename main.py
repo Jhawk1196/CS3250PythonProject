97ac5e3704987c00e7fc99016e3ee569c796039c
Jarred Hawkins <jarred.hawkins07@gmail.com>
2020-03-28 21:04:50 -0600

    Moved argument_parser.py to src directory

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from src.argument_parser import parse_args
 args = parse_args()
 # print(f"file is: {args.file}")
 # print(f"url is: {args.url}")
-#http://rss.cnn.com/rss/cnn_allpolitics.rss
-# https://xkcd.com/atom.xml
 #
+# https://xkcd.com/atom.xml
+# https://www.foxnews.com/politics.html
 
 if args.url:
     feed = parse_url_feed(args.url)
 elif args.file:
     feed = parse_url_feed(args.file)
 else:
-    feed = parse_url_feed("https://www.foxnews.com/politics.html")
+    feed = parse_url_feed("http://rss.cnn.com/rss/cnn_allpolitics.rss")
 
 helloWorld(feed)
