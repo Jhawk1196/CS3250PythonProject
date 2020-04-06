fac107524d086c6d20f9efabbe376b8a9349a264
Jarred Hawkins <jarred.hawkins07@gmail.com>
2020-04-05 20:36:25 -0600

    This should work now as well

--- src/tests/testDisplay.py
+++ src/tests/testDisplay.py
@@ -1,7 +1,6 @@
 import unittest
 import src.display as display
-"""
+
 class TestHelloWorld(unittest.TestCase):
     def runTest(self):
         self.assertEqual("https://xkcd.com/atom.xml", display.helloWorld("https://xkcd.com/atom.xml"))
-"""
