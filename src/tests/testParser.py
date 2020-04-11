8581d340d59d857c2047ad16b99c11e7a7420c33
Jarred Hawkins <jarred.hawkins07@gmail.com>
2020-04-10 21:42:00 -0600

    Added Tests for parser.py

--- src/tests/testParser.py
+++ src/tests/testParser.py
@@ -1,16 +1,16 @@
 import unittest
 from mock import Mock
 import sys
+import src.parser as parser
 
 sys.modules['atoma'] = Mock()
 sys.modules['requests'] = Mock()
 sys.modules['lxml'] = Mock()
 sys.modules['bs4'] = Mock()
 
-import src.parser as parser
 
 # @patch('parser.bs4.BeautifulSoup')
-class test_URL_Check(unittest.TestCase):
+class TestURLCheck(unittest.TestCase):
     def test_URL_Check_empty(self):
         self.assertFalse(parser.check_url(''))
 
@@ -24,7 +24,40 @@ class test_URL_Check(unittest.TestCase):
         self.assertTrue(parser.check_url('testingrss'))
 
     def test_ParseURLFeed_false(self):
-        self.assertEqual(parser.parse_url_feed('randomwords'), "Invalid URL. Must Be a RSS Feed URL ending in .rss, .html, or .xml")
+        self.assertEqual(parser.parse_url_feed('randomwords'),
+                         "Invalid URL. Must Be a RSS Feed URL ending in .rss, .html, or .xml")
 
     def test_URL_Check_incompat(self):
         self.assertFalse(parser.check_url('nevergonnagiveyouup'))
+
+
+class TestFindParser(unittest.TestCase):
+    def test_find_parser_isXML(self):
+        self.assertEqual(parser.find_parser("yayayayayyduasydiaifyaiyiafyaiyffaiyfxml"), "lxml-xml")
+
+    def test_find_parser_isTML(self):
+        self.assertEqual(parser.find_parser("sdasjhdadhausudhaudajdstml"), "lxml")
+
+    def test_find_parser_isEmpty(self):
+        self.assertEqual(parser.find_parser(""), "Invalid URL Length")
+
+
+class TestFixFeed(unittest.TestCase):
+    def test_fix_feed_isEmpty(self):
+        feed = []
+        self.assertEqual(parser.fix_feed(feed), "ERROR: FEED IS EMPTY")
+
+    def test_fix_feed_isString(self):
+        feed = ["Hello", "Hello"]
+        fixed_feed = ["Hello"]
+        self.assertEqual(parser.fix_feed(feed), fixed_feed)
+
+    def test_fix_feed_isDict(self):
+        feed = [{"Hello": "World"}, {"Hello": "World"}, {"Wonderful": "Day"}]
+        fixed_feed = [{"Hello": "World"}, {"Wonderful": "Day"}]
+        self.assertEqual(parser.fix_feed(feed), fixed_feed)
+
+    def test_fix_feed_isList(self):
+        feed = [["Hello World", "What a Wonderful Day"], ["Hello World", "What a Wonderful Day"]]
+        fixed_feed = [["Hello World", "What a Wonderful Day"]]
+        self.assertEqual(parser.fix_feed(feed), fixed_feed)
