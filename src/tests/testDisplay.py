940b4f1eafa5142bf370fabe842b6330154b2297
rheitzman4 <rheitzman4@gmail.com>
2020-04-14 12:31:24 -0600

    Unittest Font Select Class

--- src/tests/testDisplay.py
+++ src/tests/testDisplay.py
@@ -1,7 +1,7 @@
 import unittest
 import src.display as display
-"""
+
+
 class TestHelloWorld(unittest.TestCase):
-    def runTest(self):
-        self.assertEqual("https://xkcd.com/atom.xml", display.helloWorld("https://xkcd.com/atom.xml"))
-"""
+
+
