509195046c85334051b0583e95542badfa2f6eab
rheitzman4 <rheitzman4@gmail.com>
2020-04-24 13:00:19 -0600

    Unittest Font Select Class

--- tests/testfontSelect.py
+++ tests/testfontSelect.py
@@ -2,18 +2,24 @@ import unittest
 from tkinter import *
 import src.fontSelect as fontSelect
 from tkinter import font
+from src import display
+from mock import Mock, patch, MagicMock
 
 
 class testFontSelect(unittest.TestCase):
 
     def testFontStyle(self):
-        customfont = font.Font(family='Helvetica', size=12, weight="bold")
-        self.assertEqual('Times', fontSelect.fontStyle(customfont, 'Times').cget("family"))
+        root = Tk()
+        customfont = font.Font(family= 'Helvetica')
+        fontSelect.fontStyle(customfont, 'Times')
+        self.assertEqual('Times', customfont.cget('family'))
 
     def testFontSize(self):
+        root = Tk()
         customfont = font.Font(family='Helvetica', size=12)
         self.assertEqual(16, fontSelect.fontSize(customfont, 16).cget("size"))
 
     def testFontColor(self):
+        root = Tk()
         label = Message(fg='Red')
         self.assertEqual('Black', fontSelect.fontColor(label, 'Black').cget("fg"))
