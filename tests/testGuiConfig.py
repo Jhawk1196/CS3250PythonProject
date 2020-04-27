6435203290e551717153792a861a2f37a639e346
Steven Hulac <hulacst@icloud.com>
2020-04-27 15:45:47 -0600

    Added some GUI configuration import tests
    Added instructions in README.md
    
    Fixed line 41 of gui_config to check if there's a configuration argument, not just if arguments exist (there always are: the source file is always at location 0 in arguments)

--- tests/testGuiConfig.py
+++ tests/testGuiConfig.py
@@ -6,14 +6,19 @@ import src.argument_parser as argument_parser
 
 
 
-
 class TestLoadsDefault(unittest.TestCase):
-    sys.argv[1:] = ['--config', 'default_config.yml']
-    args = argument_parser.parse_args()
-    config = gui_config.Configuration(args)
+    # test to make sure it loads the default configuration appropriately
+    def test_font_size(self):
+        sys.argv[1:] = ["--config", "default_config.yml"]
+        args = argument_parser.parse_args()
+        config = gui_config.Configuration(args)
+        self.assertEqual(config.font_size, 15)
 
-    def test_fsize(self):
-        self.assertEqual(self.config.font_size, 15)
+    def test_font_color(self):
+        sys.argv[1:] = ["--config", "default_config.yml"]
+        args = argument_parser.parse_args()
+        config = gui_config.Configuration(args)
+        self.assertEqual(config.font_color, 'white')
 
 
 if __name__ == '__main__':
